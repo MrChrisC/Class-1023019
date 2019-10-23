c87cb30cc9ad6c270cbd46a0a233a6fbf71b3466
MrChrisC <38966457+MrChrisC@users.noreply.github.com>
2019-10-23 19:53:36 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Week_1_Chris_10232019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! \nThis is from Cleveland.");
+            // Console.WriteLine("This is from Cleveland");
+
+            string str1, str2;
+                        str1 = "Hello World";
+                        str2 = "Christopher";
+            Console.WriteLine(str1 +" from " + str2);
+
+            int num1, num2, num3;
+            num1 = 27;
+            num2 = 55;
+            num3 = num1 * num2;
+            
+           //Tried this break to add a line to the final outcome
+           Console.WriteLine( "\nThe product of " + num1 + " and "+ num2+ " is "+ num3);
+        }
+    }
+}
